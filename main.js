45f9a6d1b2dc70cff62b33d1a33e6855f2caa78a
alansproject <alansadewa33@gmail.com>
2025-10-25 13:07:51 +0700

    fixing object data type 2

--- main.js
+++ main.js
@@ -3,8 +3,11 @@ const btnAddBook = document.querySelector("#bookFormSubmit")
 const statusCheckBox = document.querySelector("input[type='checkbox']")
 const incompleteBookContainer = document.querySelector('#incompleteBookList')
 const completeBookContainer = document.querySelector('#completeBookList')
-let finishedBooks = JSON.parse(localStorage.getItem('finishedBooks')).map(book => ({...book, year: Number(book.year)})) || []
-let onProgressBooks = JSON.parse(localStorage.getItem('onProgressBooks')).map(book => ({...book, year: Number(book.year)})) || []
+let finishedBooks = JSON.parse(localStorage.getItem('finishedBooks')) || []
+let onProgressBooks = JSON.parse(localStorage.getItem('onProgressBooks')) || []
+
+finishedBooks = finishedBooks.map(book => ({...book, year: Number(book.year)}))
+onProgressBooks = onProgressBooks.map(book => ({...book, year: Number(book.year)}))
 
 document.addEventListener("DOMContentLoaded", () => {
     if (!JSON.parse(localStorage.getItem('finishedBooks')) || !JSON.parse(localStorage.getItem('finishedBooks')).length){
